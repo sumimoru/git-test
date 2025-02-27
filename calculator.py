0e13d521a4df8d7c07cf01a2caf8651624979cd3
sumimoru <ab22015@shibaura-it.ac.jp>
2025-02-27 10:56:47 +0900

    create sub method

--- calculator.py
+++ calculator.py
@@ -4,8 +4,13 @@ class  Calculator:
     def add(a: int, b: int) -> int:
         return a + b
     
+    @staticmethod
+    def sub(a: int, b: int) -> int:
+        return a - b
+    
 if __name__ == "__main__":
     numa = 23
     numb = 22
 
     print(Calculator.add(numa, numb))
+    print(Calculator.sub(numa, numb))
